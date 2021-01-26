7cd10c07799898e78f6dc8789e944886a8e1bc9e
andybeet <22455149+andybeet@users.noreply.github.com>
2021-01-26 12:53:50 -0500

    moved helper function in to server script

--- R/server.R
+++ R/server.R
@@ -7,12 +7,37 @@
 #' @param session session info
 #' 
 #'@importFrom shiny observeEvent
-#'@importsFrom leaflet clearGroup
+#'@importFrom leaflet clearGroup leafletProxy
 #'@importFrom magrittr %>%
+#'
+#'
 #'
 #'@export
 
 server <-  function(input, output, session) {
+  
+  ## Read in 75m depth contour (~40 fathoms)
+  contour75m <-  sf::st_read(dsn = here::here("GIS","Contours"), layer = '75m_contour',quiet=T)
+  setcrs <- sf::st_crs(contour75m)
+  
+  ## Stat Areas
+  statAreas <-  sf::st_read(dsn = here::here("GIS","Statistical_Areas"), layer = 'Statistical_Areas_Simplified',quiet=T)
+  statAreas <- sf::st_transform(statAreas,crs = setcrs)
+  ## Unique Stat Areas for selectInput
+  selectAreas <-  sort(statAreas$Id)
+  
+  ## 10min square EPUs
+  EPU10MinSq <- sf::st_read(dsn = here::here("GIS","EPU_WITH_ESTUARIES"), layer = 'EPUS_FULL',quiet=T)
+  EPU10MinSq <- sf::st_transform(EPU10MinSq,crs = setcrs)
+  
+  # Species we have data for
+  speciesList <- readRDS(here::here("data","speciesList.rds"))
+  # Location of catch by species, and year
+  gearData <- NULL
+  for (ifile in 1:length(speciesList$species)){
+    gearData[[as.character(speciesList$species[ifile])]] <- readRDS(paste0("data/",speciesList$fname[ifile]))
+  }
+  
   
 #################
 ## Leaflet map ##
